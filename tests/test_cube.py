9666ccdc2aadac2500fa2daa0b9cc6dbfe33122e
Wojciech Krzystek <wojciech.krzystek@gmail.com>
2014-05-10 14:06:56 +0200

    test(cube-operations: fitness): single rotations

--- tests/test_cube.py
+++ tests/test_cube.py
@@ -1,4 +1,5 @@
 from cube_solver.cube import Cube
+from cube_solver.rotation import F, D, L, B, U, R, Di, Li, Bi, Ui, Ri, Fi
 
 
 def test_recalculate_fitness_fresh_cube():
@@ -6,3 +7,13 @@ def test_recalculate_fitness_fresh_cube():
     c.recalculate_fitness()
 
     assert c.fitness == 0
+
+
+def test_recalculate_fitness_rotations():
+    rotations = [F, R, U, B, L, D,
+                 Fi, Ri, Ui, Bi, Li, Di]
+    for rotation in rotations:
+        c = Cube()
+        rotation(c)
+        c.recalculate_fitness()
+        assert c.fitness == 52
