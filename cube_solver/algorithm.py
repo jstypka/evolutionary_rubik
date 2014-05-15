410d35e3c3f6c3b972cd9b8d532162ae716d3830
jstypka <jasieek@student.agh.edu.pl>
2014-05-15 15:27:00 +0200

    mi & lambda fix, refactoring, debug_prinitng

--- cube_solver/algorithm.py
+++ cube_solver/algorithm.py
@@ -1,56 +1,74 @@
 # TODO(vucalur): move cube, rotations and stuff code to a separate package and leave off the alg
 from copy import deepcopy
+from time import sleep
 import random
 
 from cube_solver.cube import Cube
 from cube_solver.mutation import mutations
 
 
-INDIVIDUALS_COUNT = 1000
-LAMBDA = 50
+PARENTS = 10
+OFFSPRING = 50
 
 
-def none_solved(individuals_and_applied_rotations):
-    for pair in individuals_and_applied_rotations:
-        if pair[0].is_solved():
-            return False
-    return True
+def none_solved(population):
+    return not population[0][0].is_solved()  # first element is the best element
 
 
 def mutate(individuals_and_applied_rotations):
-    for pair in individuals_and_applied_rotations:
-        mutation = random.choice(mutations)
+    for cube, mutation_list in individuals_and_applied_rotations:
+        print("Before:", len(mutation_list))
+        mutation = random.choice(mutations)  # sometimes mutation == []
         for rotation in mutation:
-            rotation(pair[0])
-            pair[1].append(rotation)
+            rotation(cube)
+            mutation_list.append(rotation.__name__)
+        print("After:", len(mutation_list))
+        cube.recalculate_fitness()
+
+
+
+def average_mutation_length(population):
+    length_sum = 0
+    for cube, mutation_list in population:
+        length_sum += len(mutation_list)
+    return length_sum / len(population)
 
 
 def main():
-    c = Cube()
-    c.scramble()
-    c.recalculate_fitness()
+    problem = Cube()
+    problem.scramble()
 
-    individuals_and_applied_rotations = [(deepcopy(c), []) for i in range(INDIVIDUALS_COUNT)]
+    population = [(deepcopy(problem), []) for i in range(PARENTS)]
+
+    print("Original problem fitness: " + str(problem.fitness))
 
     generations = 0
-    while none_solved(individuals_and_applied_rotations):
-        mutate(individuals_and_applied_rotations)
-        for pair in individuals_and_applied_rotations:
-            pair[0].recalculate_fitness()
+    while none_solved(population) and generations < 50:
 
-        individuals_and_applied_rotations = sorted(individuals_and_applied_rotations, key=lambda pair: pair[0].fitness)
+        # reproduction
+        while len(population) < OFFSPRING:
+            population.extend(deepcopy(population))
+
+        # mutation
+        #print("Before: " + str(average_mutation_length(population)))
+        mutate(population)
+        #print("After: " + str(average_mutation_length(population)))
+
+        # sort
+        population = sorted(population, key=lambda pair: pair[0].fitness)
+
+        # selection
+        population = population[:PARENTS]
+
+        best_guy = population[0]
+        print("Generation: %d\tFitness: %d\tMutations: [%s]"
+              % (generations, best_guy[0].fitness, ', '.join(map(str, best_guy[1]))))
+        #print("Generation: %d\tPopulation: %s\tFitness: %d\tAv_mutation: %s"
+        #      % (generations, len(population), lowest_fitness, average_mutation_length(population)))
 
         generations += 1
 
-        # drop weakest LAMBDA
-        individuals_and_applied_rotations = individuals_and_applied_rotations[:-LAMBDA]
-
-        # duplicate strongest LAMBDA
-        individuals_and_applied_rotations.extend(deepcopy(individuals_and_applied_rotations[:LAMBDA]))
-
-        lowest_fitness = individuals_and_applied_rotations[0][0].fitness
-        print("Generation %d ends. Population size: %s Lowest fitness so far: %d"
-              % (generations, len(individuals_and_applied_rotations), lowest_fitness))
+        sleep(1)
 
     print("Solved in %d generations" % generations)
 
