c31f1d9b8d3b137251b2c1f999d041808563cc6f
Wojciech Krzystek <wojciech.krzystek@gmail.com>
2014-06-02 10:13:55 +0200

    test(mutation): only small changes of fitness allowed in each mutation
    117/288 - small ?!

--- tests/test_mutation.py
+++ tests/test_mutation.py
@@ -1,4 +1,5 @@
-from cube_solver.mutation import add_3_other_orientations
+from cube_solver.cube import Cube
+from cube_solver.mutation import add_3_other_orientations, mutations
 
 
 def test_add_3_other_orientations():
@@ -14,3 +15,32 @@ def test_add_3_other_orientations():
 
     add_3_other_orientations(orientations)
     assert orientations == expected
+
+
+MAX_ACCEPTED_FITNESS_CHANGE = 117
+
+
+def _each_mutation_changes_fitness_only_slightly(cube_factory):
+    for mutation in mutations:
+        cube = cube_factory()
+        fitness_before = cube.fitness
+        for rotation in mutation:
+            rotation(cube)
+        cube.recalculate_fitness()
+        fitness_after = cube.fitness
+
+        assert abs(fitness_after - fitness_before) <= MAX_ACCEPTED_FITNESS_CHANGE
+
+
+def test_each_mutation_changes_fitness_only_slightly_solved_cube():
+    cube_factory = lambda: Cube()
+    _each_mutation_changes_fitness_only_slightly(cube_factory)
+
+
+def test_each_mutation_changes_fitness_only_slightly_scrambled_cube():
+    def cube_factory():
+        cube = Cube()
+        cube.scramble()
+        return cube
+
+    _each_mutation_changes_fitness_only_slightly(cube_factory)
