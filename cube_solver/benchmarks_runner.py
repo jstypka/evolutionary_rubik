34988581a588db9ec267948a6ebc2b641add8b4b
Wojciech Krzystek <wojciech.krzystek@gmail.com>
2014-06-02 16:16:11 +0200

    fix(runner): reporting average, not last result

--- cube_solver/benchmarks_runner.py
+++ cube_solver/benchmarks_runner.py
@@ -23,6 +23,10 @@ def init_results_dict():
     return results
 
 
+def avg(array):
+    return sum(array) / len(array)
+
+
 def main():
     problem = Cube()
     problem.scramble()
@@ -32,17 +36,20 @@ def main():
         for parents in PARENTS:
             for offspring in OFFSPRING:
                 times = []
+                generations = []
+                rotations_count = []
                 for i in range(REPETITIONS):
                     start = time.process_time()
-                    generations, rotations_count = run(problem, parents, offspring, use_tournament_selection=is_tournament)
+                    _generations, _rotations_count = run(problem, parents, offspring, use_tournament_selection=is_tournament)
                     end = time.process_time()
                     times.append(end - start)
-                avg_time = sum(times) / len(times)
-                results[is_tournament][parents][offspring] = (avg_time, generations, rotations_count)
-                print(str(parents) + "," + str(offspring) + ": " + str(avg_time))
+                    generations.append(_generations)
+                    rotations_count.append(_rotations_count)
+                results[is_tournament][parents][offspring] = (avg(times), avg(generations), avg(rotations_count))
+                print(str(parents) + "," + str(offspring) + ": " + str(avg(times)))
 
     print('#' * 80 + '\n' * 2)
-    print("is_tournament parents offspring avg_time[s] generations rotations_count\n")
+    print("is_tournament parents offspring time[s] generations rotations_count\n")
     for is_tournament, v1 in results.items():
         for parents, v2 in v1.items():
             for offspring, v3 in v2.items():
